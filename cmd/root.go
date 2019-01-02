babf54a1409be3a07a4609f1710264e2bfcab039
Blerim Sheqa <blerim.sheqa@icinga.com>
2019-01-02 15:26:56 +0100

    Register includes for fields

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,9 @@ import (
 	"github.com/icinga/icingabeat/beater"
 
 	cmd "github.com/elastic/beats/libbeat/cmd"
+
+	// Register the includes.
+	_ "github.com/icinga/icingabeat/include"
 )
 
 // Name of this beat
